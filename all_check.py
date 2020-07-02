07cdb9f6451119f369fc93ff95ccab0a5f1969e8
sarthak <sarthakpahuja1603@protonmail.com>
2020-07-02 16:58:23 +0530

    To iterate over a list of checks to prevent code duplication

--- all_check.py
+++ all_check.py
@@ -10,11 +10,18 @@ def check_disk_full(disk,min_gb,min_percentage):
         return True
     return False
 
+def check_root_full():
+    """Return True if the root is true else false otherwise"""
+    return check_disk_full(disk = '/',min_gb = 2,min_percentage = 10)
+
 def main():
-    if check_disk_full(disk = '/',min_gb = 2,min_percentage = 10):
-        print("Disk full.")
-        sys.exit(1)
-    
+    checks=[
+        (check_root_full,'Root partition full'),
+    ]
+    for check , msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
     print("Everything ok.")
     sys.exit(0)
 
